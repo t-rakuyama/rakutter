c9132db26f7a5c8d8a90b55cfd98e02d3e62a1f0
koukibuu3 <koukibuu3@gmail.com>
2023-07-28 21:01:38 +0900

    style: eslintrcにlint実行

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,8 @@
 module.exports = {
   root: true,
   env: {
-    'browser': true, // windowなどのグローバル変数が認識される
-    'es2022': true // ES2022までに導入されたグローバル変数が認識される
+    browser: true, // windowなどのグローバル変数が認識される
+    es2022: true, // ES2022までに導入されたグローバル変数が認識される
   },
   extends: [
     'eslint:recommended', // ESLintの推奨ルール
@@ -13,8 +13,8 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    'ecmaVersion': 13, // JSがどの構文を使っているかをESLintに伝える
-    'sourceType': 'module' // import/exportをサポートさせる
+    ecmaVersion: 13, // JSがどの構文を使っているかをESLintに伝える
+    sourceType: 'module', // import/exportをサポートさせる
   },
   plugins: ['react-refresh'],
   rules: {
