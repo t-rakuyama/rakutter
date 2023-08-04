4434ab1f2eac0654ae9ae69b21a2c593d0d9c42c
t-rakuyama <t.rakuyama929@gmail.com>
2023-08-04 22:13:19 +0900

    Footerを表示

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 import reactLogo from '~/assets/react.svg'
 
 import viteLogo from '/vite.svg'
+
+import { Footer } from '~/components/Footer'
+
 import '~/assets/App.css'
 
 function App() {
@@ -30,6 +33,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Footer />
     </>
   )
 }
