7dd3c8f2e98b30131d96248624ac9fc3f8b91516
koukibuu3 <koukibuu3@gmail.com>
2023-08-05 23:20:22 +0900

    feat: TopページでHeaderを読み込み

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,7 @@
 import { useState } from 'react'
 
-import reactLogo from '~/assets/react.svg'
-
-import viteLogo from '/vite.svg'
-
 import { Footer } from '~/components/Footer'
+import { Header } from '~/components/Header'
 
 import '~/assets/App.css'
 
@@ -13,15 +10,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <Header />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
