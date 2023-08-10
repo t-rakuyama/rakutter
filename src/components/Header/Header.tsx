5a20ea5360bb8b960702533c273aeabcafd00ac5
koukibuu3 <koukibuu3@gmail.com>
2023-08-10 23:57:08 +0900

    feat: tailwindを使ってHeaderのデザインを修正

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,8 @@
 export const Header = () => (
   <>
-    <img src="r.svg" className="logo" alt="Vite logo" />
-    <h1>Rakutter</h1>
+    <img src="r.svg" className="inline-block h-7" alt="Vite logo" />
+    <h1 className="inline-block text-xl">
+      <span className="hidden">R</span>akutter
+    </h1>
   </>
 )
