14bd1ce6ebf3db943c0040ff298f1a2336312985
koukibuu3 <koukibuu3@gmail.com>
2023-09-21 21:21:30 +0900

    feat: oakモジュールを利用するようメイン処理を修正

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,18 @@
-const handler = (req: Request) => {
-  console.log(req)
-  return new Response('Hello World!', { status: 200 })
-}
+import { Application } from 'https://deno.land/x/oak@v12.6.1/mod.ts'
 
-Deno.serve(handler)
+import router from './router.ts'
+
+const app = new Application()
+
+app.addEventListener('listen', ({ hostname, port, secure }) => {
+  console.log(
+    `Listening on: ${secure ? 'https://' : 'http://'}${hostname ?? 'localhost'}:${port}`,
+  )
+})
+app.addEventListener('error', (e) => {
+  console.log(e.error)
+})
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+await app.listen({ port: 8000 })
