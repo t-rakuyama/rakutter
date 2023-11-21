9691677ebb43667e96d5e64e86b3e0daed5a743d
koukibuu3 <koukibuu3@gmail.com>
2023-11-21 21:46:12 +0900

    style: format実行

--- api/routes/api/docs.tsx
+++ api/routes/api/docs.tsx
@@ -1,21 +1,21 @@
 const docs = () => {
   return (
     <>
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
-        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-          <h1 class="text-4xl font-bold">API Docs</h1>
+      <div class='px-4 py-8 mx-auto bg-[#86efac]'>
+        <div class='max-w-screen-md mx-auto flex flex-col items-center justify-center'>
+          <h1 class='text-4xl font-bold'>API Docs</h1>
         </div>
       </div>
-      <div class="px-4 py-8">
+      <div class='px-4 py-8'>
         <dl>
           <dt>
-            <a href="api/joke">api/joke</a>
+            <a href='api/joke'>api/joke</a>
           </dt>
           <dd>ジョークを喋ります。</dd>
         </dl>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default docs;
+export default docs
--- app/src/pages/App.tsx
+++ app/src/pages/App.tsx
@@ -1,16 +1,14 @@
+import '~/assets/App.css'
 import { Footer } from '~/components/Footer'
 import { Header } from '~/components/Header'
 import { PostList } from '~/components/PostList'
-import '~/assets/App.css'
 
 function App() {
   return (
     <>
       <Header />
       <PostList />
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
       <Footer />
     </>
   )
