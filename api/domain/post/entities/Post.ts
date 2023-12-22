0497430fbe00bed0dc5daa937ae1ffb6d9a27728
koukibuu3 <koukibuu3@gmail.com>
2023-12-22 22:24:53 +0900

    fixup! feat: 投稿のEntityを作成

--- api/domain/post/entities/Post.ts
+++ api/domain/post/entities/Post.ts
@@ -4,31 +4,15 @@ import { PostText } from '../valueObjects/PostText.ts'
  * 投稿
  */
 export class Post {
-  private readonly id: string
-  private readonly text: PostText
-  private readonly createdAt: Date
-  private readonly updatedAt?: Date
+  public readonly id: string
+  public readonly text: PostText
+  public readonly createdAt: Date
+  public readonly updatedAt?: Date
 
   constructor(id: string, text: string, createdAt: Date, updatedAt?: Date) {
     this.id = id
     this.text = new PostText(text)
     this.createdAt = createdAt
     this.updatedAt = updatedAt
-  }
-
-  getId(): string {
-    return this.id
-  }
-
-  getText(): PostText {
-    return this.text
-  }
-
-  getCreatedAt(): Date {
-    return this.createdAt
-  }
-
-  getUpdatedAt(): Date | undefined {
-    return this.updatedAt
   }
 }
