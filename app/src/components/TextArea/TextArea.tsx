46669209bc53f22d0084e952cc4dad00722cc984
t-rakuyama <t.rakuyama929@gmail.com>
2024-05-31 22:32:33 +0900

    add: create TextAreaComponents

---
+++ app/src/components/TextArea/TextArea.tsx
@@ -0,0 +1,16 @@
+type Props = {
+  className: string
+  rows: number
+}
+;('')
+export const TextArea: React.FC<Props> = ({
+  className = 'block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+  rows = 4,
+}) => (
+  <textarea
+    id="message"
+    rows={rows}
+    className={className}
+    placeholder="Write your thoughts here..."
+  />
+)
